e19878c3efabf5e8c1c66e8cf4253822ab372627
Benjamin Siret <benjaminsiret@outlook.fr>
2023-06-29 16:10:33 +0200

    clean manifest, minor change in css

--- src/components/Pages/SignIn/SignInPage.js
+++ src/components/Pages/SignIn/SignInPage.js
@@ -34,11 +34,11 @@ export default function SignIn () {
           <form onSubmit={handleSubmit}>
             <div className="input-wrapper">
               <label htmlFor='username'>Username</label>
-              <input type="text" id="username" value={email} onChange={e => setEmail(e.target.value)} />
+              <input type="text" id="username" value={email} onChange={e => setEmail(e.target.value)} autoComplete='email' />
             </div>
             <div className="input-wrapper">
               <label htmlFor="password">Password</label>
-              <input type="password" id="password" value={password} onChange={e => setPassword(e.target.value)} />
+              <input type="password" id="password" value={password} onChange={e => setPassword(e.target.value)} autoComplete="current-password" />
             </div>
             <div className="input-remember">
               <input type="checkbox" id="remember-me" />
